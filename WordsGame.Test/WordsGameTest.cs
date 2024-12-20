99e6efcb67428553e8b09de3ef5556598dd32fb7
robinglo <glogowskirobin@gmail.com>
2024-12-20 08:50:58 +0100

    finished task 2

--- WordsGame.Test/WordsGameTest.cs
+++ WordsGame.Test/WordsGameTest.cs
@@ -3,8 +3,43 @@ namespace WordsGame.Test;
 public class WordsGameTest
 {
     [Fact]
-    public void TestNothing()
+    public void TestGradeCorrectSolution()
     {
-        Assert.True(false);
+        string input = "ABCDEFG";
+        string solution = "ABCDEFG";
+
+        WordsGame game = new WordsGame();
+        game.Start(input);
+
+        int grade = game.Grade(solution);
+
+        Assert.Equal(input.Length, grade);
+    }
+
+    [Fact]
+    public void TestGradeIncorrectSolution()
+    {
+        string input = "ABCDEFG";
+        string solution = "GFEDCBA";
+
+        WordsGame game = new WordsGame();
+        game.Start(input);
+
+        int grade = game.Grade(solution);
+
+        Assert.Equal(0, grade);
+    }
+
+    [Fact]
+    public void TestGradeNullSolution()
+    {
+        string input = "ABCDEFG";
+
+        WordsGame game = new WordsGame();
+        game.Start(input);
+
+        int grade = game.Grade(null);
+
+        Assert.Equal(0, grade);
     }
 }
